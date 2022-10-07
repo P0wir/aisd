9b5860fb73939c6a5ad58bdeaa3ae2d7bf84feda
Mateusz Powirski <92019610+P0wir@users.noreply.github.com>
2022-10-07 12:51:24 +0200

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,78 @@
+#zadanie 1
+# s1 = 'J'
+# s2 = 'Kowalski'
+# s = s1+'.'+s2
+# print(s)
+
+#zadanie 2
+# s = "Jan"
+# s2 = s[0]
+# s3 = 'Kowalski'
+# s4 = s2+'.'+s3
+# print(s4)
+
+#zadanie 3
+# x = '20'
+# y = '22'
+# z = 21
+#
+# print(int(x+y)-z)
+
+#zadanie 4
+
+# def foo(x, y, z):
+#     return int(x+y) - z
+#
+# imie = "Jan"
+# s1 = imie[0]
+# Nazwisko = "Kowalski"
+#
+# print(s1+'.'+Nazwisko, foo('20','22',57))
+
+#zadanie 5
+
+# def foo(x, y):
+#     if x > 0 and y > 0 and y != 0:
+#         return x/y
+# print(foo(5,3))
+
+#zadanie 6
+# suma=0
+# for i in range(0,1000):
+#     suma += int(input("podaj liczbe"))
+#     if suma < 100:
+#         continue
+#     break
+
+#zadanie 7
+# l = ["y","b","c","d"]
+# print(l)
+# def foo(x):
+#     return tuple(x)
+# print(foo(l))
+
+#zadanie 8
+lista = []
+for i in range(0,1000):
+    x = input("element listy")
+    if x !=0:
+        lista.append(x)
+        continue
+    break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
