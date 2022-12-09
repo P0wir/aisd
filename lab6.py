203b52236a01d6bf059e9f70e6762c45a40ebcf9
Mateusz Powirski <92019610+P0wir@users.noreply.github.com>
2022-12-09 02:09:37 +0100

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -1,4 +1,6 @@
-from typing import Any, Callable
+from __future__ import annotations
+from graphviz import *
+from typing import Any, Callable, List
 
 
 class BinaryNode:
@@ -27,6 +29,16 @@ class BinaryNode:
             return
         self.right_child = BinaryNode(value)
 
+    def show(self, g=Digraph('g')):
+        g.node(str(self), str(self.value))
+        if self.left_child:
+            g.edge(str(self), str(self.left_child))
+            self.left_child.show(g)
+        if self.right_child:
+            g.edge(str(self), str(self.right_child))
+            self.right_child.show(g)
+        return g
+
     def __str__(self) -> str:
         return str(self.value)
 
@@ -37,32 +49,40 @@ class BinarySearchTree:
     def __init__(self, root: 'BinaryNode') -> None:
         self.root = root
 
+    def __insert(self, node: BinaryNode, value: Any) -> BinaryNode:
+        if node:
+            if value < node.value:
+                node.left_child = self.__insert(node.left_child, value)
+            else:
+                node.right_child = self.__insert(node.right_child, value)
+        else:
+            node = BinaryNode(value)
+        return node
+
     def insert(self, value: Any) -> None:
         if self.root is None:
             self.root = BinaryNode(value)
-        if self.root.value < value:
-            self.root.right_child = root._insert(value)
-        
+        else:
+            self.root = self.__insert(self.root, value)
 
-    # def _insert(self, value: Any) -> BinaryNode:
-    #     if self.root is None:
-    #         self.root = BinaryNode(value)
-    #         return root
-    #     else:
-    #         if self.root.value == value:
-    #             return self.root
-    #         elif self.root.value < value:
-    #             self.root.right_child = self.insert(value)
-    #         else:
-    #             self.root.left_child = self.insert(value)
+    def insert_list(self, list_: List[Any]) -> None:
+        for i in list_:
+            self.insert(i)
+
+    def remove(self, value: Any) -> None:
+        if self.root is None:
+            self.root = BinaryNode(value)
+        else:
+            self.root = self.__remove(self.root, value)
+
+    def show(self) -> None:
+        self.root.show().render(filename='bst', view=True)
 
 
 root = BinaryNode(10)
-root.add_left_child(7)
-root.left_child.add_left_child(3)
-root.left_child.left_child.add_left_child(2)
-root.add_right_child(15)
 korzen = BinarySearchTree(root)
 print(korzen)
-print(root.min())
-korzen.insert(5)
+korzen.insert_list([11,15,1,2,5,7,3,8,21,4,16,36,12])
+korzen.show()
+
+print(korzen)
